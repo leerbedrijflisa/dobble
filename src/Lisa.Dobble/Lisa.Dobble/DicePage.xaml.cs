1bf084c9d1acde2dc56c6b1cb89e7fdd3ffc7e9f
Reverp <contact@reverbs.pw>
2014-10-08 23:37:11 +0200

    Added RollDice method
    Layout needs work though

--- src/Lisa.Dobble/Lisa.Dobble/DicePage.xaml.cs
+++ src/Lisa.Dobble/Lisa.Dobble/DicePage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Lisa.Dobble.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,16 +10,53 @@ namespace Lisa.Dobble
 {
     public partial class DicePage
     {
+        private Dice dice;
         public DicePage()
         {
             InitializeComponent();
 
             NavigationPage.SetHasNavigationBar(this, false);
+
+            CreateSampleDice();
+
+            var tapGestureRecognizer = new TapGestureRecognizer();
+            tapGestureRecognizer.TappedCallback += (s, e) =>
+            {
+                RollDice();
+            };
+
+            MainGrid.GestureRecognizers.Add(tapGestureRecognizer);
         }
 
-        private void Roll()
+        private void CreateSampleDice()
         {
+            dice = new Dice();
+            dice.Options = new List<Option>();
 
+            for(var i = 0; i < 6; i++)
+            {
+                var option = new Option();
+
+                if(i == 0)
+                {
+                    option.Image = "dice.png";
+                }else{
+                    option.Image = String.Format("dice{0}.png", i + 1);
+                }
+
+                dice.Options.Add(option);
+            }
+
+            dice.Name = "Sample";
+        }
+
+        private void RollDice()
+        {
+            var random = new Random();
+            int randomNumber = random.Next(0, dice.Options.Count());
+
+            DiceImage.Source = dice.Options[randomNumber].Image;
+            this.InvalidateMeasure();
         }
     }
 }
