e459f7debc6dfc0d904b5571af40f9b3a3aa665c
Josja de Greef <josjadegreef@gmail.com>
2014-10-09 15:03:31 +0200

    Created Profilemenu page

---
+++ src/Lisa.Dobble/Lisa.Dobble/ProfileMenuPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Lisa.Dobble
+{
+    public partial class ProfileMenuPage
+    {
+        public ProfileMenuPage()
+        {
+            InitializeComponent();
+
+            diceImage.Source =  Device.OnPlatform(
+                iOS: ImageSource.FromFile("Dice/dice.png"),
+                Android:  ImageSource.FromFile("Drawable/dice.png"),
+                WinPhone: ImageSource.FromFile("dice.png"));
+        }
+    }
+}
