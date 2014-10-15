b2bcf0892b710433a2d086fb1e02911fb6b6c5dc
Reverp <contact@reverbs.pw>
2014-10-15 16:18:51 +0200

    Made the ProfilePage work with the database

--- src/Lisa.Dobble/Lisa.Dobble/ProfileMenuPage.xaml.cs
+++ src/Lisa.Dobble/Lisa.Dobble/ProfileMenuPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Lisa.Dobble.Data;
+using Lisa.Dobble.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,14 +11,48 @@ namespace Lisa.Dobble
 {
     public partial class ProfileMenuPage
     {
+        DieDatabase database;
+        IEnumerable<Die> dice;
         public ProfileMenuPage()
         {
             InitializeComponent();
+            database = new DieDatabase();
+            dice = database.GetDice();
+            foreach(Die die in dice)
+            {
+                TextCell dieCell = new TextCell();
+                dieCell.Text = die.Name;
+                dieCell.ClassId = die.Id.ToString();
+                dieCell.Tapped += dieCell_Tapped;
+                ProfileSection.Add(dieCell);
+            }
+        }
 
-            diceImage.Source =  Device.OnPlatform(
-                iOS: ImageSource.FromFile("Dice/dice.png"),
-                Android:  ImageSource.FromFile("Drawable/dice.png"),
-                WinPhone: ImageSource.FromFile("dice.png"));
+        private void dieCell_Tapped(object sender, EventArgs e)
+        {
+            var tappedDieCell = (TextCell)sender;
+            SetDie(int.Parse(tappedDieCell.ClassId));
+        }
+
+        private void SetDie(int dieId)
+        {
+            var selectedDie = dice.Where(x => x.Id == dieId).FirstOrDefault();
+            SetImages(selectedDie);
+        }
+
+        private void SetImages(Die die)
+        {
+            var count = 1;
+            foreach(var imageObject in ProfileGrid.Children.OfType<Image>())
+            {
+                var dieImage = (Image)imageObject;
+                dieImage.Source = Device.OnPlatform (
+                    iOS: ImageSource.FromFile("Dice/dice" + count + ".png"),
+                    Android: ImageSource.FromFile("Drawable/dice"  + count + ".png"),
+                    WinPhone: ImageSource.FromFile("dice" + count + ".png"));
+
+                count++;
+            }
         }
     }
 }
