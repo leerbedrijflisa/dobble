2c3089c66ec2c2217810d8eec3fde396142e1ad9
Reverp <contact@reverbs.pw>
2014-10-16 15:33:12 +0200

    Implemented FileManager

---
+++ src/Lisa.Dobble/Lisa.Dobble.iOS/FileManager.cs
@@ -0,0 +1,74 @@
+using Lisa.Dobble;
+using Lisa.Dobble.iOS;
+using System;
+using System.IO;
+using Xamarin.Forms;
+using Xamarin.Forms.Labs.Services.IO;
+
+[assembly: Dependency(typeof(FileManager))]
+namespace Lisa.Dobble.iOS
+{
+    public class FileManager : IFileManager
+    {
+        public bool DirectoryExists(string path)
+        {
+            string fullPath = CreateFullPath(path);
+            return Directory.Exists(fullPath);
+        }
+
+        public void CreateDirectory(string path)
+        {
+            string fullPath = CreateFullPath(path);
+            Directory.CreateDirectory(fullPath);
+        }
+
+        public Stream OpenFile(string path, Xamarin.Forms.Labs.Services.IO.FileMode mode, Xamarin.Forms.Labs.Services.IO.FileAccess access)
+        {
+            string fullPath = CreateFullPath(path);
+            return File.Open(fullPath, TranslateFileMode(mode), TranslateFileAccess(access));
+        }
+
+        public bool FileExists(string path)
+        {
+            string fullPath = CreateFullPath(path);
+            return File.Exists(fullPath);
+        }
+
+        public DateTimeOffset GetLastWriteTime(string path)
+        {
+            string fullPath = CreateFullPath(path);
+            return File.GetLastWriteTime(fullPath);
+        }
+
+        private string CreateFullPath(string path)
+        {
+            var folder = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            return String.Format("{0}/{1}", folder, path);
+        }
+
+        private System.IO.FileMode TranslateFileMode(Xamarin.Forms.Labs.Services.IO.FileMode mode)
+        {
+            switch (mode)
+            {
+                case Xamarin.Forms.Labs.Services.IO.FileMode.CreateNew: return System.IO.FileMode.CreateNew;
+                case Xamarin.Forms.Labs.Services.IO.FileMode.Create: return System.IO.FileMode.Create;
+                case Xamarin.Forms.Labs.Services.IO.FileMode.Open: return System.IO.FileMode.Open;
+                case Xamarin.Forms.Labs.Services.IO.FileMode.OpenOrCreate: return System.IO.FileMode.OpenOrCreate;
+                case Xamarin.Forms.Labs.Services.IO.FileMode.Truncate: return System.IO.FileMode.Truncate;
+                case Xamarin.Forms.Labs.Services.IO.FileMode.Append: return System.IO.FileMode.Append;
+                default: throw new NotImplementedException();
+            }
+        }
+
+        private System.IO.FileAccess TranslateFileAccess(Xamarin.Forms.Labs.Services.IO.FileAccess access)
+        {
+            switch (access)
+            {
+                case Xamarin.Forms.Labs.Services.IO.FileAccess.Read: return System.IO.FileAccess.Read;
+                case Xamarin.Forms.Labs.Services.IO.FileAccess.Write: return System.IO.FileAccess.Write;
+                case Xamarin.Forms.Labs.Services.IO.FileAccess.ReadWrite: return System.IO.FileAccess.ReadWrite;
+                default: throw new NotImplementedException();
+            }
+        }
+    }
+}
---
+++ src/Lisa.Dobble/Lisa.Dobble/Interfaces/IFileManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xamarin.Forms.Labs.Services.IO
+{
+    public interface IFileManager
+    {
+        bool DirectoryExists(string path);
+
+        void CreateDirectory(string path);
+
+        Stream OpenFile(string path, FileMode mode, FileAccess access);
+
+        bool FileExists(string path);
+
+        DateTimeOffset GetLastWriteTime(string path);
+    }
+
+    public enum FileMode
+    {
+        CreateNew = 1,
+        Create = 2,
+        Open = 3,
+        OpenOrCreate = 4,
+        Truncate = 5,
+        Append = 6,
+    }
+
+    public enum FileAccess
+    {
+        Read = 1,
+        Write = 2,
+        ReadWrite = 3,
+    }
+}
