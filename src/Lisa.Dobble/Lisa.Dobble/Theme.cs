fe5f70e95b3019c532ff68d71c571b92f0829b0a
Josja de Greef <josjadegreef@gmail.com>
2014-12-12 11:39:03 +0100

    replaced resource dictionary with theme class

---
+++ src/Lisa.Dobble/Lisa.Dobble/Theme.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Lisa.Dobble
+{
+    public static class Theme
+    {
+        public static FileImageSource ButtonImage = Device.OnPlatform(
+                        iOS: "info-48.png",
+                        Android: "Drawable/info.png",
+                        WinPhone: "info.png");
+    }
+}
