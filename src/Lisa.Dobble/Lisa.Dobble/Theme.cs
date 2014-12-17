a34747d9e068fe28cf36cdb4905193aba1756c81
Josja de Greef <josjadegreef@gmail.com>
2014-12-17 10:53:16 +0100

    changed imagebuttons to buttons with theme class

--- src/Lisa.Dobble/Lisa.Dobble/Theme.cs
+++ src/Lisa.Dobble/Lisa.Dobble/Theme.cs
@@ -13,5 +13,29 @@ namespace Lisa.Dobble
                         iOS: "info-48.png",
                         Android: "Drawable/info.png",
                         WinPhone: "info.png");
+        public static FileImageSource CreditsButton = Device.OnPlatform(
+                        iOS: "info-48.png",
+                        Android: "Drawable/info.png",
+                        WinPhone: "info.png");
+        public static ImageSource Logo = Device.OnPlatform(
+                        iOS: "Logo.png",
+                        Android: "Drawable/logo.png",
+                        WinPhone: "Logo.png");
+        public static ImageSource DobbleMask = Device.OnPlatform(
+                        iOS:"dobblemask_small.png",
+                        Android:"Drawable/dobblemasksmall.png",
+                        WinPhone:"dobblemasksmall.png");
+        public static FileImageSource PlayButton = Device.OnPlatform(
+                       iOS: "play.png",
+                       Android: "Drawable/play.png",
+                       WinPhone: "play.png");
+        public static FileImageSource RecordButton = Device.OnPlatform(
+                       iOS: "record.png",
+                       Android: "Drawable/record.png",
+                       WinPhone: "record.png");
+        public static ImageSource Timer = Device.OnPlatform(
+                        iOS: "tick.png",
+                        Android: "Drawable/tick.png",
+                        WinPhone: "tick.png");
     }
 }
