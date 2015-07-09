0174a6c95be21b87150536541769ba95cb67b2c8
Joost Ronkes Agerbeek <raj@davinci.nl>
2015-07-09 10:26:00 +0200

    Improved touch sensor for rolling die.

--- src/Lisa.Dobble/Lisa.Dobble.iOS/FancyIosFrameRenderer.cs
+++ src/Lisa.Dobble/Lisa.Dobble.iOS/FancyIosFrameRenderer.cs
@@ -12,47 +12,14 @@ namespace Lisa.Dobble.iOS
 {
     public class FancyIosFrameRenderer : FrameRenderer
     {
-        FancyFrame frame;
+        public override void TouchesEnded(Foundation.NSSet touches, UIEvent evt)
+		{
+			base.TouchesEnded(touches, evt);
 
-        protected override void OnElementChanged(ElementChangedEventArgs<Frame> e)
-        {
-            base.OnElementChanged(e);
-            frame = (FancyFrame)this.Element;
-            //var tapGestureRecognizer = new UITapGestureRecognizer(HandleSwipe);
-            
-            var swipeGestureRecognizer = new UISwipeGestureRecognizer(HandleSwipe);
-            swipeGestureRecognizer.Direction = UISwipeGestureRecognizerDirection.Down | UISwipeGestureRecognizerDirection.Up;
-
-            var swipeGestureRecognizer2 = new UISwipeGestureRecognizer(HandleSwipe);
-            swipeGestureRecognizer2.Direction = UISwipeGestureRecognizerDirection.Left | UISwipeGestureRecognizerDirection.Right;
-
-            var swipeGestureRecognizer3 = new UISwipeGestureRecognizer(HandleSwipe);
-            swipeGestureRecognizer3.Direction = UISwipeGestureRecognizerDirection.Down | UISwipeGestureRecognizerDirection.Up;
-            swipeGestureRecognizer3.NumberOfTouchesRequired = 2;
-
-            var swipeGestureRecognizer4 = new UISwipeGestureRecognizer(HandleSwipe);
-            swipeGestureRecognizer4.Direction = UISwipeGestureRecognizerDirection.Left | UISwipeGestureRecognizerDirection.Right;
-            swipeGestureRecognizer4.NumberOfTouchesRequired = 2;
-
-            var swipeGestureRecognizer5 = new UISwipeGestureRecognizer(HandleSwipe);
-            swipeGestureRecognizer5.Direction = UISwipeGestureRecognizerDirection.Down | UISwipeGestureRecognizerDirection.Up;
-            swipeGestureRecognizer5.NumberOfTouchesRequired = 3;
-
-            var swipeGestureRecognizer6 = new UISwipeGestureRecognizer(HandleSwipe);
-            swipeGestureRecognizer6.Direction = UISwipeGestureRecognizerDirection.Left | UISwipeGestureRecognizerDirection.Right;
-            swipeGestureRecognizer6.NumberOfTouchesRequired = 3;
-
-            this.AddGestureRecognizer(swipeGestureRecognizer);
-            this.AddGestureRecognizer(swipeGestureRecognizer2);
-            this.AddGestureRecognizer(swipeGestureRecognizer3);
-            this.AddGestureRecognizer(swipeGestureRecognizer4);
-            this.AddGestureRecognizer(swipeGestureRecognizer5);
-            this.AddGestureRecognizer(swipeGestureRecognizer6);
-        }
-
-        private void HandleSwipe()
-        {
-            frame.SendSwipe();
-        }
+			// Any kind of touch should roll the die, so whenever a gesture ends -
+			// no matter which one - we simulate a swipe.
+			FancyFrame frame = (FancyFrame) this.Element;
+			frame.SendSwipe();
+		}
     }
 }
